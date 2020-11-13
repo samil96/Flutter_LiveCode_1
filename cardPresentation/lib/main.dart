6fadb1c77a0ea9d9463cbafadaedfc065a64f0ad
samil96 <1628112@utp.edu.pe>
2020-11-12 23:19:15 -0500

    widget card-ListTile-Image.asset

--- cardPresentation/lib/main.dart
+++ cardPresentation/lib/main.dart
@@ -11,7 +11,7 @@ class LomoSaltado extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Color(0xff018786),
+        backgroundColor: Color(0xff00C4B4),
         body: SafeArea(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -20,6 +20,9 @@ class LomoSaltado extends StatelessWidget {
                 radius: 50,
                 backgroundImage: NetworkImage("https://pbs.twimg.com/profile_images/1180657921493016577/kZn1QGVE_400x400.jpg"), 
               ),
+              SizedBox(
+                height: 10,
+              ),
               Text(
                 "@Lesly.Samaritano",
                 style: TextStyle(
@@ -27,6 +30,9 @@ class LomoSaltado extends StatelessWidget {
                   color: Color(0xffF2E7FE),
                   fontWeight: FontWeight.bold,
                 ),
+              ),
+              SizedBox(
+                height: 5,
               ),
               Text(
                 "FLUTTER DEVELOPER",
@@ -37,10 +43,91 @@ class LomoSaltado extends StatelessWidget {
                   letterSpacing: 3,
                 ),
               ),
-              Divider(
-                thickness: 0.4,
+              SizedBox(
+                width: 160,
+                height: 30,
+                child: Divider(
+                thickness: 0.9,
                 color: Color(0xffF2E7FE),
-              )
+                ),
+              ),
+              Card(
+                color: Color(0xffF2E7FE),
+                margin: EdgeInsets.symmetric(horizontal: 15),
+                child: ListTile(
+                leading: Icon(
+                  Icons.phone_android, 
+                  size: 32, 
+                  color: Color(0xff5600E8),
+                ),
+                title: Text("+51 960084505",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xff23036A),
+                  ),
+                ),
+                //subtitle: Text("Subtitulo"),
+                trailing: Icon(
+                  Icons.check, 
+                  size: 32,
+                  color: Color(0xff5600E8),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Card(
+                color: Color(0xffF2E7FE),
+                margin: EdgeInsets.symmetric(horizontal: 15),
+                child: ListTile(
+                leading: Icon(
+                  Icons.mail, 
+                  size: 32, 
+                  color: Color(0xff5600E8),
+                ),
+                title: Text("lesly.samaritano.ayala@gmail.com",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xff23036A),
+                  ),
+                ),
+                //subtitle: Text("Subtitulo"),
+                trailing: Icon(
+                  Icons.check, 
+                  size: 32,
+                  color: Color(0xff5600E8),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                   Image.asset(
+                    "assets/images/linkedin.png",
+                    width: 40,
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Image.asset(
+                    "assets/images/instagram.png",
+                    width: 40,
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Image.asset(
+                    "assets/images/facebook.png",
+                    width: 40,
+                  ),
+                ],
+              ),
             ],
           ),
         )
